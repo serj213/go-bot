bc608e080d778d7dbf021df40dcbd5c79144255a
serj213 <kornilovsergey55@gmail.com>
2024-12-18 16:35:31 +0300

    start telegram bot

--- cmd/go-bot/main.go
+++ cmd/go-bot/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	token := "7298916047:AAFKcjNGNUs3de6ysCmnlGta3pc6NYBfttM"
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Лизка пуська "+ update.Message.Text)
+			// msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-bot
 
 go 1.22.5
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
